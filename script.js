c014605f9fdb7770c0b66aa513434a26ebbdb55d
lalala080 <janiellashane08@gmail.com>
2025-11-14 05:49:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+const result = document.getElementById("result");
+
+function append(value) {
+  result.value += value;
+}
+
+function clearDisplay() {
+  result.value = "";
+}
+
+function deleteLast() {
+  result.value = result.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    result.value = eval(result.value);
+  } catch {
+    result.value = "Error";
+  }
+}
+
 
